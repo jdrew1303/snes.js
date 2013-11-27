3422d6fd42b89808b88cb0da982e921ea3bcb01e
Chad Engler <englercj@live.com>
2013-11-27 08:27:54 -0800

    starting to add optables

--- src/cpu/Cpu.js
+++ src/cpu/Cpu.js
@@ -1,13 +1,35 @@
 // 65816 Processor Emulator
-var Registers = require('./Registers');
+var Registers = require('./Registers'),
+    Opcodes = require('./Opcodes');
 
 var Cpu = function() {
     this.regs = new Registers();
-    this.aa = 0;
-    this.rd = 0;
-    this.sp = 0;
-    this.dp = 0;
 
-    this.opdata = null;
+    this.aa = 0; //24-bit reg
+    this.rd = 0; //24-bit reg
+    this.sp = 0; //8-bit uint
+    this.dp = 0; //8-bit uint
+
+    //the map table that holds which function to run to emulate an opcode
+    this.optable = null;
 };
 
+Cpu.prototype.constructor = Cpu;
+
+Cpu.prototype.updateTable = function() {
+    if(this.regs.e) {
+        this.optable = Opcodes.OPTABLE_EM;
+    } else if(this.regs.p.m) {
+        if(this.regs.p.x) {
+            this.optable = Opcodes.OPTABLE_MX;
+        } else {
+            this.optable = Opcodes.OPTABLE_Mx;
+        }
+    } else {
+        if(this.regs.p.x) {
+            this.optable = Opcodes.OPTABLE_mX;
+        } else {
+            this.optable = Opcodes.OPTABLE_mx;
+        }
+    }
+};
